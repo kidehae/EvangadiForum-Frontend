d3f5607781c1a2bce6f3bf90e7266305a13669c5
girmayevangadi <girmayabrha@gmail.com>
2025-06-11 11:56:57 -0700

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Footer from "./Components/Footer/Footer"
+import { Route,Routes } from 'react-router-dom'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+     <Routes>
+
+<Route path="/" element={<Footer/>}/>
+     </Routes>
+      
+
+    </div>
   )
 }
 
