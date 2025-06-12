bb26f78ea260bd3d1d4ff18a252e9b14f81dd556
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-13 00:29:03 +0300

    Added question list card

--- src/Components/QuestionList/QuestionList.jsx
+++ src/Components/QuestionList/QuestionList.jsx
@@ -1,101 +1,40 @@
-import React from "react";
-import classes from "./QuestionList.module.css";
-import { AccountCircle } from "@mui/icons-material";
-import PersonPinIcon from "@mui/icons-material/PersonPin";
-import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
-import QuestionPage from "../../Pages/Question/QuestionPage";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from "./QuestionList.module.css";
+import { FaUserCircle, FaAngleRight } from "react-icons/fa";
+import PropTypes from "prop-types";
 
-function QuestionList() {
-  const navigate = useNavigate();
+const QuestionList = ({ username, title, questionId }) => {
   return (
-    <div className={classes.container}>
-      <section className={classes.header}>
-        <div className={classes.headerContent}>
-          <button
-            className={classes.button}
-            onClick={() => navigate("/ask-question")}
-          >
-            Ask Question
-          </button>
-          <p className={classes.welcome}>Welcome: danny_gir_admin</p>
+    <div className={styles.questionsContainer}>
+      <hr />
+      <div className={styles.askQuestion}>
+        <div className={styles.askUserInfo}>
+          <div className={styles.askUser}>
+            <Link to={`/question/${questionId}`}>
+              <FaUserCircle className={styles.icon} size={65} />
+            </Link>
+            <span className={styles.username}>{username}</span>
+          </div>
+          <div className={styles.askQuestionText}>
+            <p>{title}</p>
+          </div>
         </div>
-      </section>
-      <section className={classes.questionsSection}>
-        <h1>Questions</h1>
-        <div className={classes.questionsList}>
-          <ul>
-            <li className={classes.questionItem}>
-              <div className={classes.questionContent}>
-                <div className={classes.userIconandname}>
-                  {" "}
-                  <PersonPinIcon className={classes.icon} fontSize="0px" />
-                  <p className={classes.user}>mistake_Z</p>
-                </div>
-                <div className={classes.questionText}>git gen menderiew?</div>
-              </div>
-              <KeyboardArrowRightIcon
-                className={classes.arrow}
-                onClick={() => {
-                  navigate("/singlequestion-page");
-                }}
-              />
-            </li>
-            <li className={classes.questionItem}>
-              <div className={classes.questionContent}>
-                <div className={classes.userIconandname}>
-                  {" "}
-                  <PersonPinIcon className={classes.icon} />{" "}
-                  <p className={classes.user}>danny_gir_admin</p>
-                </div>
-                <div className={classes.questionText}>
-                  what's react-router-dom?
-                </div>
-              </div>
-              <KeyboardArrowRightIcon
-                className={classes.arrow}
-                onClick={() => {
-                  navigate("/singlequestion-page");
-                }}
-              />
-            </li>
-            <li className={classes.questionItem}>
-              <div className={classes.questionContent}>
-                <div className={classes.userIconandname}>
-                  {" "}
-                  <PersonPinIcon className={classes.icon} />{" "}
-                  <p className={classes.user}>danny_gir_admin</p>
-                </div>
-                <div className={classes.questionText}>what is http?</div>
-              </div>
-              <KeyboardArrowRightIcon
-                className={classes.arrow}
-                onClick={() => {
-                  navigate("/singlequestion-page");
-                }}
-              />
-            </li>
-            <li className={classes.questionItem}>
-              <div className={classes.questionContent}>
-                <div className={classes.userIconandname}>
-                  {" "}
-                  <PersonPinIcon className={classes.icon} />{" "}
-                  <p className={classes.user}>reduit_12</p>
-                </div>
-                <div className={classes.questionText}>What's dir?</div>
-              </div>
-              <KeyboardArrowRightIcon
-                className={classes.arrow}
-                onClick={() => {
-                  navigate("/singlequestion-page");
-                }}
-              />
-            </li>
-          </ul>
+        <div className={styles.askArrow}>
+          <Link to={`/question/${questionId}`}>
+            <FaAngleRight className={styles.icon} size={25} />
+          </Link>
         </div>
-      </section>
+      </div>
+      <hr />
     </div>
   );
-}
+};
+
+QuestionList.propTypes = {
+  username: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  questionId: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+};
 
 export default QuestionList;
